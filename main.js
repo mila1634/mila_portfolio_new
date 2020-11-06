bbbd26cfaab522d4b9c67b252aacbb760ac28867
Mila Perkovic <milaburgcher@gmail.com>
2020-11-06 02:00:07 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+console.clear();
+
+const app = (() => {
+	let body;
+	let menu;
+	let menuItems;
+	
+	const init = () => {
+		body = document.querySelector('body');
+		menu = document.querySelector('.menu-icon');
+		menuItems = document.querySelectorAll('.nav__list-item');
+
+		applyListeners();
+	}
+	
+	const applyListeners = () => {
+		menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+	}
+	
+	const toggleClass = (element, stringClass) => {
+		if(element.classList.contains(stringClass))
+			element.classList.remove(stringClass);
+		else
+			element.classList.add(stringClass);
+	}
+	
+	init();
+})();
+
+
+//scroll up
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
